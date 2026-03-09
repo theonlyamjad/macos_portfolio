9bd7ccc26d9365e2108777b22a9f851ad5cc4f50
theonlyamjad <amjadmessi2004@gmail.com>
2026-03-09 13:00:09 +0000

    adding pdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Navbar, Welcome ,Dock,HomeDesktop } from "./components";
 import { Draggable } from "gsap/Draggable";
 import { gsap } from "gsap";
 import {Terminal,Safari,Resume,Finder,Text,Image,Contact,Photos} from "@/windows";
-import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(Draggable);
 
 export default function Home() {
